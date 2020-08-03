9767853cee17368041c3aecadfb769c28e5a7e4e
Markay12 <54555076+Markay12@users.noreply.github.com>
2020-08-03 16:45:50 -0700

    completed app routing

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return 'Hello World!'
 
+@app.route('/name')
+def name():
+    return 'My name is Mark'
+
 if __name__ == "__main__":
     app.run(debug=True)
