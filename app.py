b899479a72ce6656232325c1a1f3314f8e3b7fd6
Mark Ashinhust <ashinhust.brass@gmail.com>
2020-08-03 19:09:00 -0700

    implement html files

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/index')
 def index():
-    return '<h1> Index Page </h1>'
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
