9181698fac5b358c2a87ffd886858aba4e729d07
Mark Ashinhust <ashinhust.brass@gmail.com>
2020-08-03 19:20:14 -0700

    import database

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+#db == database
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False) #cannot create blank task
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
 
 @app.route('/index')
 def index():
