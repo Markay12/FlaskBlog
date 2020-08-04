c3ad8d6afc25eab7174beb072c00b551e3d3e4c0
Mark Ashinhust <ashinhust.brass@gmail.com>
2020-08-03 18:55:39 -0700

    removed index scripting and html files

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return 'Hello World! <h1>Hello!!</h1>'
+
+@app.route('/<name>')
+def user(name):
+    return f"Hello {name.title()}!"
+
+@app.route("/admin")
+def admin():
+    return redirect(url_for("home"))
 
 if __name__ == "__main__":
     app.run(debug=True)
