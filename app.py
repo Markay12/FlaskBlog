203d5254f227f0c40335e3810dbe91ba9a7f3a43
Mark Ashinhust <ashinhust.brass@gmail.com>
2020-08-03 19:42:02 -0700

    first database

--- app.py
+++ app.py
@@ -10,12 +10,13 @@ db = SQLAlchemy(app)
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False) #cannot create blank task
+    completed = db.Column(db.Integer, default=0)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return '<Task %r>' % self.id
 
-@app.route('/index')
+@app.route('/')
 def index():
     return render_template('index.html')
 
