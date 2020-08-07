42a51381f4f86d5c22b3868e4578e86dde30bb6d
Markay12 <54555076+Markay12@users.noreply.github.com>
2020-08-06 19:33:49 -0700

    first posts with dummy data

--- app.py
+++ app.py
@@ -1,14 +1,45 @@
-from flask import Flask
+from flask import Flask, render_template
 # __name__ == special module in python
 app = Flask(__name__)
 
+# post library that allows us to keep track of user information and posts
+# used in home.html to post and understand the template and output
+posts = [
+
+    {
+
+        'author': 'Mark Ashinhust',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 06, 2020'
+
+
+    },
+
+    {
+
+        'author': 'Mark Ashinhust',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 06, 2020'
+
+
+    },
+
+
+]
+
+
 @app.route("/")
-def hello_world():
-    return '<h1>Hello World!</h1>'
+@app.route('/home')
+# multiple routes to the same webpage localhost:5000/home and localhost:5000
+def home():
+    #returns our html template from templates folder
+    return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
-    return '<h1>About Me!</h1>'
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
