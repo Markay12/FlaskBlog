d2e6d582611a7b446a6a7bb1ee8015a35a884dd0
Mark Ashinhust <ashinhust.brass@gmail.com>
2020-08-07 13:02:04 -0700

    added extra title to about page

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def home():
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('home.html', title='About')
 
 if __name__ == '__main__':
     app.run(debug=True)
