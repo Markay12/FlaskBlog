12873909667503fa53f62a28ea7bef477ec72523
Markay12 <54555076+Markay12@users.noreply.github.com>
2020-08-06 19:18:29 -0700

    route to about page

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return '<h1>Hello World!</h1>'
 
+@app.route('/about')
+def about():
+    return '<h1>About Me!</h1>'
+
 if __name__ == '__main__':
     app.run(debug=True)
