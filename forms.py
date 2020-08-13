d8d77d79cf351a8a3011b1a254f13b75b458bdcc
Mark Ashinhust <ashinhust.brass@gmail.com>
2020-08-13 10:33:20 -0700

    edit to login form text

--- forms.py
+++ forms.py
@@ -23,5 +23,5 @@ class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
-    submit = SubmitField('Sign Up!')
+    submit = SubmitField('Log In')
     
